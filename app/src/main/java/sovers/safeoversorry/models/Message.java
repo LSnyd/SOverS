8441f4adda9a4fda55c02db6f434cd1b24bf6a09
fsommer95 <36508949+fsommer95@users.noreply.github.com>
2018-06-20 14:48:53 +0200

    Update Message.java
    enlarged the message class. added speed, position and status

--- app/src/main/java/sovers/safeoversorry/models/Message.java
+++ app/src/main/java/sovers/safeoversorry/models/Message.java
@@ -9,12 +9,19 @@ public class Message {
     private String message;
     private String user_id;
     private String timestamp;
+    private String status;
+    private float speed;
+    private float distance;
     private android.location.Location position;
 
-    public Message(String message, String user_id, String timestamp) {
+    public Message(String message, String user_id, String timestamp, String status, float speed, float distance) {
         this.message = message;
         this.user_id = user_id;
         this.timestamp = timestamp;
+        this.status = status;
+        this.speed = speed;
+        this.distance = distance;
+
     }
 
     public Message() {
@@ -49,12 +56,41 @@ public class Message {
         this.timestamp = timestamp;
     }
 
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public float getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(float speed) {
+        this.speed = speed;
+    }
+
+    public float getDistance() {
+        return distance;
+    }
+
+    public void setDistance(float distance) {
+        this.distance = distance;
+    }
+
+
+
     @Override
     public String toString() {
         return "Message{" +
                 "message='" + message + '\'' +
                 "position='" + position + '\'' +
                 ", user_id='" + user_id + '\'' +
+                "status='" + status + '\'' +
+                "speed='" + speed + '\'' +
+                ", distance='" + distance + '\'' +
                 ", timestamp='" + timestamp + '\'' +
                 '}';
     }
